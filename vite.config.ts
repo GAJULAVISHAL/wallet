28c35c7e41715a6774d2d9954aabae2b422877c0
sirikancharla12 <sirikancharla1290@gmail.com>
2025-05-18 14:57:19 +0530

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [react(),
     tailwindcss(),
-    react()
-  ],
+    nodePolyfills()
+     ],
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  }
+  
 })
